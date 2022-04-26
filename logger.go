bcece619205a9650b73b52584ec6bef3f730d809
Nat Welch <nat@natwelch.com>
2022-04-26 16:48:06 +0000

    allow for other users to use logger

--- logger.go
+++ logger.go
@@ -4,32 +4,30 @@ import (
 	"fmt"
 )
 
-// logger is a logger interface compatible with both stdlib and some
+// Logger is a logger interface compatible with both stdlib and some
 // 3rd party loggers.
-type logger interface {
+type Logger interface {
 	Output(int, string) error
 }
 
 // ilogger represents the internal logging api we use.
 type ilogger interface {
-	logger
+	Logger
+
 	Print(...interface{})
 	Printf(string, ...interface{})
 	Println(...interface{})
-}
-
-type Debug interface {
-	Debug() bool
 
 	// Debugf print a formatted debug line.
 	Debugf(format string, v ...interface{})
+
 	// Debugln print a debug line.
 	Debugln(v ...interface{})
 }
 
 // internalLog implements the additional methods used by our internal logging.
 type internalLog struct {
-	logger
+	Logger
 }
 
 // Println replicates the behaviour of the standard logger.
@@ -47,14 +45,12 @@ func (t internalLog) Print(v ...interface{}) {
 	t.Output(2, fmt.Sprint(v...))
 }
 
-type discard struct{}
-
-func (t discard) Debug() bool {
-	return false
+// Debugf print a formatted debug line.
+func (t internalLog) Debugf(format string, v ...interface{}) {
+	t.Output(2, fmt.Sprint(v...))
 }
 
-// Debugf print a formatted debug line.
-func (t discard) Debugf(format string, v ...interface{}) {}
-
 // Debugln print a debug line.
-func (t discard) Debugln(v ...interface{}) {}
+func (t internalLog) Debugln(v ...interface{}) {
+	t.Output(2, fmt.Sprint(v...))
+}
