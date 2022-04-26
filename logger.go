8815f4ccd4bff18170aec4737eb7dfaf027425bb
Nat Welch <nat@natwelch.com>
2022-04-26 18:14:43 +0000

    logger

--- logger.go
+++ logger.go
@@ -1,9 +1,5 @@
 package slack
 
-import (
-	"fmt"
-)
-
 // Logger is a logger interface compatible with both stdlib and some 3rd party
 // loggers.
 type Logger interface {
@@ -13,37 +9,6 @@ type Logger interface {
 	Println(...interface{})
 	Debugf(format string, v ...interface{})
 	Debugln(v ...interface{})
-}
-
-// InternalLog implements the additional methods used by our internal logging
-// ontop of Output().
-type InternalLog struct {
-	Logger
-}
-
-// Println replicates the behaviour of the standard logger.
-func (t InternalLog) Println(v ...interface{}) {
-	t.Output(2, fmt.Sprintln(v...))
-}
-
-// Printf replicates the behaviour of the standard logger.
-func (t InternalLog) Printf(format string, v ...interface{}) {
-	t.Output(2, fmt.Sprintf(format, v...))
-}
-
-// Print replicates the behaviour of the standard logger.
-func (t InternalLog) Print(v ...interface{}) {
-	t.Output(2, fmt.Sprint(v...))
-}
-
-// Debugf print a formatted debug line.
-func (t InternalLog) Debugf(format string, v ...interface{}) {
-	t.Output(2, fmt.Sprint(v...))
-}
-
-// Debugln print a debug line.
-func (t InternalLog) Debugln(v ...interface{}) {
-	t.Output(2, fmt.Sprint(v...))
 }
 
 type discard struct{}
--- logger_test.go
+++ logger_test.go
@@ -8,6 +8,37 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// internalLog implements the additional methods used by our internal logging
+// ontop of Output().
+type internalLog struct {
+	Logger
+}
+
+// Println replicates the behaviour of the standard logger.
+func (t internalLog) Println(v ...interface{}) {
+	t.Output(2, fmt.Sprintln(v...))
+}
+
+// Printf replicates the behaviour of the standard logger.
+func (t internalLog) Printf(format string, v ...interface{}) {
+	t.Output(2, fmt.Sprintf(format, v...))
+}
+
+// Print replicates the behaviour of the standard logger.
+func (t internalLog) Print(v ...interface{}) {
+	t.Output(2, fmt.Sprint(v...))
+}
+
+// Debugf print a formatted debug line.
+func (t internalLog) Debugf(format string, v ...interface{}) {
+	t.Output(2, fmt.Sprint(v...))
+}
+
+// Debugln print a debug line.
+func (t internalLog) Debugln(v ...interface{}) {
+	t.Output(2, fmt.Sprint(v...))
+}
+
 func TestLogging(t *testing.T) {
 	buf := bytes.NewBufferString("")
 	logger := internalLog{Logger: log.New(buf, "", 0|log.Lshortfile)}
